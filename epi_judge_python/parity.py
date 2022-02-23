ce058e8738284fad8db061ee9d386237a2ffae42
Connor-McMillin <22306123+Connor-McMillin@users.noreply.github.com>
2022-02-22 16:03:03 -0800

    Finished parity

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    lookup = {
+        0: 0,
+        1: 1,
+        2: 1,
+        3: 0
+    }
+
+    result = 0
+    while x:
+        result ^= lookup[x & 0b11]
+        x >>= 2
+
+    return result
 
 
 if __name__ == '__main__':
