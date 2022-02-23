bca3b82ed628f36e0ef476bef92813b23d82457a
Connor-McMillin <22306123+Connor-McMillin@users.noreply.github.com>
2022-02-22 16:04:14 -0800

    finished reverse_bits

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -2,9 +2,23 @@ from test_framework import generic_test
 
 
 def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    reversed_int = 0
 
+    lookup = {
+        0: 0,
+        1: 2,
+        2: 1,
+        3: 3
+    }
+
+    bit_mask = 0b11
+    mask_size = 2
+
+    for i in reversed(range(64 // mask_size)):
+        reversed_int |= lookup[x & bit_mask] << (mask_size * i)
+        x >>= mask_size
+
+    return reversed_int
 
 if __name__ == '__main__':
     exit(
