19ace4bb8ea39cabcee6300ce204ce9ee50a29e4
Connor-McMillin <22306123+Connor-McMillin@users.noreply.github.com>
2022-02-22 16:03:27 -0800

    finished primitive_multiply

--- epi_judge_python/primitive_multiply.py
+++ epi_judge_python/primitive_multiply.py
@@ -1,9 +1,18 @@
 from test_framework import generic_test
 
+def add(x: int, y: int) -> int:
+    return x if y == 0 else add(x ^ y, (x & y) << 1)
 
 def multiply(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    result = 0
+
+    while x:
+        if x & 1:
+            result = add(result, y)
+
+        x, y = x >> 1, y << 1
+
+    return result
 
 
 if __name__ == '__main__':
