d1cee4f4dd2a7ede6be9d0535d56ceb70ee463f3
Connor-McMillin <22306123+Connor-McMillin@users.noreply.github.com>
2022-02-22 16:05:27 -0800

    Finished swap_bits

--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -2,8 +2,13 @@ from test_framework import generic_test
 
 
 def swap_bits(x, i, j):
-    # TODO - you fill in here.
-    return 0
+    ith_bit = (x >> i) & 1
+    jth_bit = (x >> j) & 1
+
+    if ith_bit == jth_bit:
+        return x
+    else:
+        return x ^ (1 << i | 1 << j)
 
 
 if __name__ == '__main__':
